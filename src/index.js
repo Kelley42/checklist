0fd3da4bd7a36d517075a32e694a4d09a80b85e3
Kelley42 <kelleykristine42@yahoo.com>
2022-10-21 17:06:18 -0500

    Added hide/show navbar button, fixed flexbox elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import { todos } from './todo';
 
 todos();
+
+function toggleNavbar() {
+    if ((navbarContainer.style.display == "none") == true) {
+        show_navbar();
+    } else {
+        hide_navbar();
+    }
+}
+
+function hide_navbar() {
+    navbarContainer.style.display = "none";
+}
+
+function show_navbar() {
+    navbarContainer.style.display = "block";
+}
+
+const navbarContainer = document.querySelector("#navbar-container");
+const navbarButton = document.querySelector("#nav-button-expand");
+navbarButton.addEventListener("click", toggleNavbar);
+
+hide_navbar();
