0781bbd58db6b7428cb51cd2186dc585a6d292ee
Kelley42 <kelleykristine42@yahoo.com>
2022-10-22 16:18:43 -0500

    Fixed ability to transform opacity and position of navbar

--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
 import { todos } from './todo';
 
 function toggleNavbar() {
-    if (navbarContainer.classList.contains("hidden") == true) {
-        console.log("show me")
-        show_navbar();
-    } else {
-        console.log("hide me")
-        hide_navbar();
-    }
+    navbarContainer.classList.toggle("visible");
+    // if (navbarContainer.classList.contains("hidden") == true) {
+    //     console.log("show me")
+    //     show_navbar();
+    // } else {
+    //     console.log("hide me")
+    //     hide_navbar();
+    // }
 }
 
-function hide_navbar() {
-    if(navbarContainer.classList.contains("visible")) {
-        navbarContainer.classList.remove("visible");
-    } else{ //to start with
-        navbarContainer.style.display = "none"; 
-    }
-    navbarContainer.classList.add("hidden");
-    navbarContainer.visibility = "hidden";
-}
+// function hide_navbar() {
+//     if(navbarContainer.classList.contains("visible")) {
+//         navbarContainer.classList.remove("visible");
+//     } else{ //to start with
+//         navbarContainer.style.display = "none"; 
+//     }
+//     navbarContainer.classList.add("hidden");
+//     navbarContainer.visibility = "hidden";
+// }
 
-function show_navbar() {
-    navbarContainer.style.display = "block";
-    navbarContainer.classList.remove("hidden");
-    navbarContainer.classList.add("visible");
-    navbarContainer.visibility = "visible";
-}
+// function show_navbar() {
+//     navbarContainer.style.display = "block";
+//     navbarContainer.classList.remove("hidden");
+//     navbarContainer.classList.add("visible");
+//     navbarContainer.visibility = "visible";
+// }
 
 const navbarContainer = document.querySelector("#navbar-container");
 const navbarButton = document.querySelector("#nav-button-expand");
 navbarButton.addEventListener("click", toggleNavbar);
 
-hide_navbar();
+//hide_navbar();
 todos();
