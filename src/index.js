f9beb506fdce48dbe0b0b019a7be88f529d511d0
Kelley42 <kelleykristine42@yahoo.com>
2022-10-22 17:07:52 -0500

    Added edit images and fixed position for html

--- src/index.js
+++ src/index.js
@@ -2,35 +2,11 @@ import { todos } from './todo';
 
 function toggleNavbar() {
     navbarContainer.classList.toggle("visible");
-    // if (navbarContainer.classList.contains("hidden") == true) {
-    //     console.log("show me")
-    //     show_navbar();
-    // } else {
-    //     console.log("hide me")
-    //     hide_navbar();
-    // }
 }
 
-// function hide_navbar() {
-//     if(navbarContainer.classList.contains("visible")) {
-//         navbarContainer.classList.remove("visible");
-//     } else{ //to start with
-//         navbarContainer.style.display = "none"; 
-//     }
-//     navbarContainer.classList.add("hidden");
-//     navbarContainer.visibility = "hidden";
-// }
-
-// function show_navbar() {
-//     navbarContainer.style.display = "block";
-//     navbarContainer.classList.remove("hidden");
-//     navbarContainer.classList.add("visible");
-//     navbarContainer.visibility = "visible";
-// }
 
 const navbarContainer = document.querySelector("#navbar-container");
 const navbarButton = document.querySelector("#nav-button-expand");
 navbarButton.addEventListener("click", toggleNavbar);
 
-//hide_navbar();
 todos();
