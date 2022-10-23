b19295fa098c996083ac6f8b1e2a8a07d8d46a07
Kelley42 <kelleykristine42@yahoo.com>
2022-10-23 16:15:52 -0500

    Added popup window for new project form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import { todos } from './todo';
 
-function toggleNavbar() {
-    navbarContainer.classList.toggle("visible");
+function hideAddProjectForm() {
+    form.style.display = "none";
 }
 
+function showAddProjectForm() {
+    form.style.display = "block";
+}
 
 const navbarContainer = document.querySelector("#navbar-container");
 const navbarButton = document.querySelector("#nav-button-expand");
-navbarButton.addEventListener("click", toggleNavbar);
+navbarButton.addEventListener("click", () => navbarContainer.classList.toggle("visible"));
 
+const addProject = document.querySelector("#add-project");
+addProject.addEventListener("click", showAddProjectForm);
+const form = document.querySelector(".new-project-form")
+const cancel_project_btn = document.querySelector("#cancel-project-btn");
+cancel_project_btn.addEventListener("click", hideAddProjectForm);
+const add_project_btn = document.querySelector("#save-project-btn");
+//add_book_btn.addEventListener("click", addNewBook);
+
+hideAddProjectForm();
 todos();
